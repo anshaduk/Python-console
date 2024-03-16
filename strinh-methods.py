dd58d1d000fafefb912e6904a94f675b1f988f47
Anshad UK <anshadu@gmail.com>
2024-03-16 17:16:49 +0530

    python console

--- strinh-methods.py
+++ strinh-methods.py
@@ -9,9 +9,17 @@
 # a1="2hello"
 # print(a1.isalpha())#False
 
-email="anshad123@gmail.com"
-print(email.islower())
-print(email.upper())
-print(email.isalnum())
+# email="anshad123@gmail.com"
+# print(email.islower())
+# print(email.upper())
+# print(email.isalnum())
 
-print('i am anshad'.isalnum())
+# print('i am anshad'.isalnum())
+
+#index()
+str="Hello this is a super man"
+print(str.index('e'))
+print(str.rindex('e'))
+
+print("I love python".count('o'))
+# print("I love Python".index('p'))
